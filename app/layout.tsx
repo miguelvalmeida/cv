3d39ed31e4bbac27461b9966cfb2db2a92547325
Miguel Almeida <miguel.almeida@thetrainline.com>
2024-01-14 17:08:18 +0000

    Remove speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { PropsWithChildren } from 'react'
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Inter } from 'next/font/google'
 
 import './globals.css'
@@ -12,7 +11,6 @@ export default function RootLayout({ children }: PropsWithChildren) {
     <html lang="en">
       <body className={inter.className}>{children}</body>
       <Analytics />
-      <SpeedInsights />
     </html>
   )
 }

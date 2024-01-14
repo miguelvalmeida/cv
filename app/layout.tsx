22c19a1d04ef2e98d37d26b16b48410cfd724dac
Miguel Almeida <miguel.almeida@thetrainline.com>
2024-01-14 16:53:50 +0000

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
+import type { PropsWithChildren } from 'react'
+import { Analytics } from '@vercel/analytics/react'
 import { Inter } from 'next/font/google'
+
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   )
 }
